baa34b35378f89d88919bb60a592983dfe1d4c68
406PC-19 <406PC-19@PC-18>
2023-11-24 09:23:11 +0900

    scanner 실습

--- chapter03/src/ddit/chap03/sec01/ScannerTest.java
+++ chapter03/src/ddit/chap03/sec01/ScannerTest.java
@@ -11,7 +11,7 @@ public class ScannerTest {
 //		int i = sc.nextInt();
 //		int result = a + i;
 //		System.out.println("입력 받은 숫자 " + i + "와 숫자 " + a + "를 더한 결과 값은 " + result);
-				
+		
 		System.out.print("문자를 입력해주세요: ");
 //		String s = sc.next();
 //		System.out.println("입력 받은 문자는 " + "\"" + s + "\"" + " 입니다.");
